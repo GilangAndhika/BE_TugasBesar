568ceacef0e45cfb4d60b81e3542c43e1be7b448
barganakukuhraditya <barganakukuh@gmail.com>
2024-07-21 18:01:08 +0700

    bson tahun_peluncuran

--- module/fungsi.go
+++ module/fungsi.go
@@ -32,14 +32,14 @@ func InsertOneDoc(db string, collection string, doc interface{}) (insertedID int
 
 func InsertParfume(db *mongo.Database, col string, nama string, jenis string, merk string, deskripsi string, harga int, thn int, stok int, ukuran string) (insertedID primitive.ObjectID, err error) {
 	parfume := bson.M{
-		"nama_parfume":    	nama,
-		"jenis_parfume":    jenis,
-		"merk":     		merk,
-		"deskripsi": 		deskripsi,
-		"harga":     		harga,
-		"thn_peluncuran":   thn,
-		"stok":      		stok,
-		"ukuran":      		ukuran,
+		"nama_parfume":    		nama,
+		"jenis_parfume":    	jenis,
+		"merk":     			merk,
+		"deskripsi": 			deskripsi,
+		"harga":     			harga,
+		"tahun_peluncuran":   	thn,
+		"stok":      			stok,
+		"ukuran":      			ukuran,
 	}
 	result, err := db.Collection(col).InsertOne(context.Background(), parfume)
 	if err != nil {
@@ -81,14 +81,14 @@ func UpdateParfume(_id primitive.ObjectID, db *mongo.Database, col string, nama 
 	filter := bson.M{"_id": _id}
 	update := bson.M{
 		"$set": bson.M{
-			"nama_parfume":    	nama,
-			"jenis_parfume":    jenis,
-			"merk":     		merk,
-			"deskripsi": 		deskripsi,
-			"harga":     		harga,
-			"thn_peluncuran":   thn,
-			"stok":      		stok,
-			"ukuran":      		ukuran,
+			"nama_parfume":    		nama,
+			"jenis_parfume":    	jenis,
+			"merk":     			merk,
+			"deskripsi": 			deskripsi,
+			"harga":     			harga,
+			"tahun_peluncuran":   	thn,
+			"stok":      			stok,
+			"ukuran":      			ukuran,
 		},
 	}
 	result, err :=db.Collection(col).UpdateOne(context.Background(), filter, update)
